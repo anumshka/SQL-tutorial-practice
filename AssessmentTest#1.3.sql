ef4068d38fa775d8e8574a3c10ef30dd79234d87
anumshka <anushka.mpathak17@gmail.com>
2021-08-05 16:16:54 +0530

    edit AssessmentTest#1.3

--- AssessmentTest#1.3.sql
+++ AssessmentTest#1.3.sql
@@ -1,5 +1,5 @@
 -- What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500
-SELECT * FROM customer
+SELECT first_name,last_name FROM customer
 WHERE first_name LIKE 'E%'
 AND address_id<500
 ORDER BY customer_id DESC
