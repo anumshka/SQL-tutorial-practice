3331cad59ce373d3541f2fea15a0426a234edbb9
anumshka <anushka.mpathak17@gmail.com>
2021-08-05 16:13:06 +0530

    add AssessmentTest#1

---
+++ AssessmentTest#1.1.sql
@@ -0,0 +1,5 @@
+--Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2
+SELECT customer_id,SUM(amount) FROM payment
+WHERE staff_id=2
+GROUP BY customer_id
+HAVING SUM(amount)>=110;
---
+++ AssessmentTest#1.3.sql
@@ -0,0 +1,6 @@
+-- What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500
+SELECT * FROM customer
+WHERE first_name LIKE 'E%'
+AND address_id<500
+ORDER BY customer_id DESC
+LIMIT 1;
