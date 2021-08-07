5fa3421282376ebd71d7b1eef06024cad8080590
anumshka <anushka.mpathak17@gmail.com>
2021-08-08 01:30:48 +0530

    add AssessmentTest#2

---
+++ AssessmentTest#2.sql
@@ -0,0 +1,27 @@
+/*CREATE TABLE students(
+student_id SERIAL PRIMARY KEY, 
+first_name VARCHAR(10) NOT NULL,
+last_name VARCHAR(10) NOT NULL, 
+homeroom_number INTEGER, 
+phone VARCHAR(20) UNIQUE NOT NULL,
+email VARCHAR(20) UNIQUE, 
+graduation_year INTEGER   
+)*/
+/*CREATE TABLE teachers(
+teacher_id SERIAL PRIMARY KEY, 
+first_name VARCHAR(10) NOT NULL,
+last_name VARCHAR(10) NOT NULL, 
+homeroom_number INTEGER, 
+phone VARCHAR(20) UNIQUE NOT NULL,
+email VARCHAR(20) UNIQUE, 
+department VARCHAR(20)   
+)*/
+-- Once you've made the tables, insert a student named Mark Watney (student_id=1) who has a phone number of 777-555-1234 and doesn't have an email. He graduates in 2035 and has 5 as a homeroom number.
+/*INSERT INTO students(first_name,last_name, 
+homeroom_number,phone,graduation_year)
+VALUES ('Mark','Watney',5,'7755551234',2035);*/
+-- Then insert a teacher names Jonas Salk (teacher_id = 1) who as a homeroom number of 5 and is from the Biology department. His contact info is: jsalk@school.org and a phone number of 777-555-4321.
+/*INSERT INTO teachers(first_name,last_name, 
+homeroom_number,department,email,phone)
+VALUES ('Jonas','Salk',5,'Biology','jsalk@school.org','7755554321');*/
+
